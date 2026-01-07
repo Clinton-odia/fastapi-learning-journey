93b0e574142213eb753301c16f9f48f54b619bae
Clintonodia <odiamehiclinton@gmail.com>
2026-01-07 14:12:09 +0100

    Completed mini project

--- 01-task-manager/main.py
+++ 01-task-manager/main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from pydantic import BaseModel
+from fastapi import status
+from fastapi import HTTPException
 app = FastAPI()
 
 
@@ -37,12 +39,18 @@ data = [
 def welcome():
     return {"message" : "Welcome to my todo-list app"}
 # Create task endpoint
-@app.post("/tasks")
+@app.post("/tasks",status_code=status.HTTP_201_CREATED)
 def create_tasks(task : Task):
-    data.append(task)
+    data.append(task.model_dump())
     return {"message" : "task has been created"}
 #Read a single task
-
+@app.get("/tasks/{id}")
+def list_single_task(id : int):
+    for task in data:
+        if task["id"] == id:
+            return {"detail" : task}
+    
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not found!")
 
 #Read all the tasks
 
@@ -50,5 +58,21 @@ def create_tasks(task : Task):
 def get_all_tasks(): 
     return {"detail" : data }
 #Update task
-
+@app.put("/tasks/{id}", status_code=status.HTTP_200_OK)
+def update_task(id : int, task: Task):
+    for item in data:
+        if item["id"] == id:
+            item.update(task.model_dump())
+            
+            return {"message" : "updated task",
+                    "task" : item}
+    
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
 #Delete tasks
+@app.delete("/tasks/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_task(id : int):
+    for i, item in enumerate(data):
+        if item["id"] == id:
+            data.pop(i)
+            return Response(status_code=status.HTTP_204_NO_CONTENT)
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="task don't exist")
